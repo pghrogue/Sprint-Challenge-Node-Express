cf97d279452d8462a85b13f8c056e84dc5bd4893
Jennifer King <jenbellpgh@gmail.com>
2018-12-08 13:48:51 -0500

    Added post method to api/projects with error checking

--- Routes/projectsRouter.js
+++ Routes/projectsRouter.js
@@ -36,7 +36,8 @@ router.get( '/:id', customMW.checkValidProject, (req, res) => {
   // end projects
 });
 
-// Get actions:
+// Get actions - may need to come back to this later. Need to add POST first to
+// check for what happens when no actions are available for valid project:
 router.get( '/:id/actions', customMW.checkValidProject, (req, res) => {
   const { id } = req.params;
 
@@ -52,7 +53,29 @@ router.get( '/:id/actions', customMW.checkValidProject, (req, res) => {
 });
 
 // POST:
-router.post( '/', (req, res) => {});
+router.post( '/', (req, res) => {
+  const projectData = req.body;
+
+  // Check for empty required fields:
+  if( !projectData.name || !projectData.description ) {
+    res.status(400).json({ error: "Please provide name and description of the project."});
+  } else {
+    // Check for empty completed field:
+    if( !projectData.completed ) {
+      projectData.completed = 0;
+    }
+    // Insert:
+    projects
+      .insert(projectData)
+      .then( newProject => {
+        res.json(newProject);
+      })
+      .catch( err => {
+        res.status(500).json({ error: "Could not add new project."});
+      });
+    // end projects
+  }
+});
 
 // PUT:
 router.put( '/:id', (req, res) => {});
