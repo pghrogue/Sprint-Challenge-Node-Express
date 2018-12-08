d8c412ac8f9fd07bb10fb51492b49b3467b6edc0
Jennifer King <jenbellpgh@gmail.com>
2018-12-08 12:57:40 -0500

    Finished boilerplate for projectsRouter.js

--- Routes/projectsRouter.js
+++ Routes/projectsRouter.js
@@ -3,17 +3,25 @@ const express = require('express');
 const router = express.Router();
 
 // App requires:
+const projects = require('../data/helpers/projectModel');
+
 
 /* ---------- Endpoints for /api/projects ---------- */
 // GET (list):
+router.get( '/', (req, res) => {});
 
 // GET by id:
+router.get( '/:id', (req, res) => {});
 
 // POST:
+router.post( '/', (req, res) => {});
 
 // PUT:
+router.put( '/:id', (req, res) => {});
 
 // DELETE:
+router.delete( '/:id', (req, res) => {});
+
 
 /* ---------- Export ---------- */
 module.exports = router;
